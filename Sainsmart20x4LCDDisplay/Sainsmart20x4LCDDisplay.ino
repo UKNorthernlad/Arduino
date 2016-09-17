0f4877bef33da6e9c4be9d2be25336e94c52dba4
Northernlad <blah@blah.com>
2016-09-17 23:29:47 +0100

    Added I2C LCD sample code.

---
+++ Sainsmart20x4LCDDisplay/Sainsmart20x4LCDDisplay.ino
@@ -0,0 +1,53 @@
+/*
+ Example Arduino sketch for
+ SainSmart IIC/I2C/TWI Serial 2004 20x4 LCD Module Shield For Arduino UNO MEGA R3
+ http://www.sainsmart.com/sainsmart-iic-i2c-twi-serial-2004-20x4-lcd-module-shield-for-arduino-uno-mega-r3.html
+ 
+
+ This example uses F Malpartida's NewLiquidCrystal library. Obtain from:
+ https://bitbucket.org/fmalpartida/new-liquidcrystal 
+ 
+*/
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
+
+#define I2C_ADDR      0x3F // I2C address of PCF8574A
+#define BACKLIGHT_PIN 3
+#define En_pin        2
+#define Rw_pin        1
+#define Rs_pin        0
+#define D4_pin        4
+#define D5_pin        5
+#define D6_pin        6
+#define D7_pin        7
+
+LiquidCrystal_I2C twilcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin, BACKLIGHT_PIN, POSITIVE);
+
+
+void setup(){
+
+  twilcd.begin(16,2);
+  twilcd.home();
+  //1234567890123456
+  //I2C/TWI BackPack
+  twilcd.print("I2C/TWI BackPack");
+  twilcd.setBacklight(HIGH);
+  delay(3000);
+  twilcd.setBacklight(LOW);
+  delay(3000);
+  twilcd.setBacklight(HIGH);
+  delay(3000);
+  twilcd.setBacklight(LOW);
+  delay(3000);
+  twilcd.setBacklight(HIGH);
+  //twilcd.clear();
+};
+//////////////////////////////////////////////////////////////
+
+
+void loop(){
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
