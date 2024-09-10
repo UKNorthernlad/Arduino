abea85160a7224e2b2006db688c2f6f1e367937e
Billy Bunter <billy@bunter.com>
2024-09-10 17:03:32 +0100

    Reformatted output

--- ArduinoEEPROMReadWrite/ArduinoEEPROMReadWrite.ino
+++ ArduinoEEPROMReadWrite/ArduinoEEPROMReadWrite.ino
@@ -10,33 +10,27 @@ void setup() {
   EraseEEPROM();
   WriteString();
   //ReadString();
-  PrintHexDump();
+  HexDumpEEPROM();
 }
 
-void EraseEEPROM()
-{
-    for (int i = 0 ; i < EEPROM.length() ; i++) {
-    EEPROM.write(i, 0);
+void EraseEEPROM() {
+  for (int i = 0; i < EEPROM.length(); i++) {
+    EEPROM.write(i, 0xFF);
   }
 }
 
-void WriteString()
-{
-  char message[] = "Hello World";
+void WriteString() {
+  String message = "Hello World";
   int address = 0;
 
-  byte len = sizeof(message);
-
-  for (int i = 0; i < len; i++)
-
+  for (int i = 0; i < (message.length())+1; i++) // returns the string size with terminating null
   {
     EEPROM.write(address + i, message[i]);
   }
 }
 
 // A basic way to loop through all EEPROM addresses and print out their value.
-void ReadString()
-{
+void ReadString() {
   int index = 0;
 
   Serial.begin(9600);
@@ -59,51 +53,46 @@ void ReadString()
 
 // Hex dump version of ReadString()
 // Taken from https://forum.arduino.cc/t/eeprom-hexdump-strange-characters-at-beginning-of-serial-output-solved/179066
-void PrintHexDump()
-{ 
-  unsigned long addr = 0;                              // used fro address in EEPROM
-  byte i,j,c;                                          // i=1 to 16 bytes, j=HEX index, c= single character
-  char buffer[22];   
+void HexDumpEEPROM() {
+  unsigned long addr = 0;  // used fro address in EEPROM
+  byte c = 0;              // i=1 to 16 bytes, j=HEX index, c= single character
+  short j = 0;
+  char buffer[16];
 
-  Serial.begin(9600);                            // initalize serial port
-  buffer[0] = ' ';                               // put space at beginning of char array
-  buffer[1] = ' ';                               // put second space at beginning of char array
-  buffer[18] =0;                                 // string termination for buffer
-  buffer[19] =0;                                 // belts and suspenders
-  Serial.println();
-  Serial.println();
-  Serial.println (F("EEPROM DUMP:"));            // information, notice the F() wrapper
-  do
-  { /* Read 16 characters from the EEprom into our buffer */
+  Serial.begin(9600);
+  Serial.println("EEPROM DUMP:");
+  do { /* Read 16 characters from the EEprom into our buffer */
     /* ------ Print the address padded with spaces ------ */
-    //if (addr<1000) {Serial.print(' ');}          // space padding, char(32) is a space
-    if (addr<256) {Serial.print(' ');}           // space padding
-    if (addr<16) {Serial.print(' ');}            // space padding
-    Serial.print (addr, HEX);                    // starting address for line
-    Serial.print ("  ");                         // seperator, could be replaced with a tab ("\t")
-    for (i=0; i<16; ++i)                         // 16 bytes at a time
-      {  c= EEPROM.read(addr + i);               // read one byte
-         buffer[i+2]=char(c);                      // stuff the character in our buffer
-         /*--- print HEX value ---*/
-         if (c < 0x10) {Serial.print('0');}      // pad HEX value with 0, 0x10 = F0 = 16
-         Serial.print (c, HEX);                  // using HEX tells Serial how to format the number
-         ++j;                                    // increment index for serperator
-         if (j == 4)                             // if we have print 4 values
-           {  Serial.print(' ');                 // print a seperator 
-              j=0;                               // set our index to zero to start over
-           }
-         /*--- Evaluate character ---*/
-         if (c < 32) {buffer[i+2]='.';}          // do not print control codes
-         if (c ==92) {buffer[i+2]='.';}          // do not print "\"
-         if (c >126) {buffer[i+2]='.';}          // do not print 'extended' characters
+    if (addr < 1000) { Serial.print('0'); }  // space padding, char(32) is a space
+    if (addr < 256) { Serial.print('0'); }   // space padding
+    if (addr < 16) { Serial.print('0'); }    // space padding
+    Serial.print(addr, HEX);                 // starting address for line
+    Serial.print("  ");                      // seperator, could be replaced with a tab ("\t")
+    for (short i = 0; i < 16; ++i)           // 16 bytes at a time
+    {
+      c = EEPROM.read(addr + i);  // read one byte
+      buffer[i] = char(c);        // stuff the character in our buffer
+      /*--- print HEX value ---*/
+      if (c < 0x10) { Serial.print('0'); }  // pad HEX value with 0, 0x10 = F0 = 16
+      Serial.print(c, HEX);                 // using HEX tells Serial how to format the number
+      ++j;                                  // increment index for serperator
+      if (j == 4)                           // if we have print 4 values
+      {
+        Serial.print(' ');  // print a seperator
+        j = 0;              // set our index to zero to start over
       }
-    Serial.println(buffer);                      // begin wiht a new line
-    addr=addr +16;
-  } while (addr < E2END);   // E2END is the end of the EEPROM
+      /*--- Evaluate character ---*/
+      if (c < 32) { buffer[i] = '.'; }   // do not print control codes
+      if (c == 92) { buffer[i] = '.'; }  // do not print "\"
+      if (c > 126) { buffer[i] = '.'; }  // do not print 'extended' characters
+    }
+    Serial.println(buffer);  // Print out the 16byte buffer in ASCII format
+    addr = addr + 16;
+  } while (addr < E2END);  // E2END is the end of the EEPROM
   /*  used to check ending address
   Serial.println(addr,DEC);
   Serial.println(E2END,DEC);
-  */ 
+  */
 }
 
 void loop() {}
